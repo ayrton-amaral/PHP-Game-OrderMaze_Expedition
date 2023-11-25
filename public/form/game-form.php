f2c254af7642e948df2d71f14117d2ca4e2ec0c2
Bruno Landeiro <landeiro.b@gmail.com>
2023-11-25 13:36:02 -0500

    timeout signout ajuste

--- public/form/game-form.php
+++ public/form/game-form.php
@@ -2,11 +2,6 @@
 <head>
 <?php 
 include_once __DIR__ . '\..\template\head.php';
-include_once '../../src/features/signout-timeout.php';
-//session_start();
-if(empty($_SESSION["user"])){
-    header('Location: signin-form.php'); 
-}
 ?>
  <title>Game</title>
     <style>
@@ -51,34 +46,11 @@ if(empty($_SESSION["user"])){
           </div>
           <button id = "buttonSubmit" onclick="checkAnswer(event)" >Submit answer</button>
         </form>
-    <input id="countDownDate" type="hidden" value=<?=$_SESSION["expire"] *1000; ?>>
-    <p id="demo"></p>
   </div>
 </body>
 </html>
 
 <script>
-  // Set the date we're counting down to
-  var countDownDate = document.getElementById("countDownDate").value;;
-  // Update the count down every 1 second
-  var x = setInterval(function() {
-  // Get today's date and time
-  var now = new Date().getTime();
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = minutes + ":" + seconds;
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
 
   let currentLevel = 1;
   let correctGenerated = [];
--- public/template/nav.php
+++ public/template/nav.php
@@ -1,3 +1,12 @@
+<?php 
+include_once '../../src/features/signout-timeout.php';
+if(empty($_SESSION["user"])){
+    header('Location: signin-form.php'); 
+} else {
+    $_SESSION['start'] = time(); 
+    $_SESSION['expire'] = $_SESSION['start'] + (900) ;//900 = 15 minutes 
+}
+?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
         <a class="navbar-brand" href="<?= INDEX ?>">Kids Games</a>
@@ -13,9 +22,27 @@
                     <a class="nav-link" href="#">Link</a>
                 </li>
             </ul>
+            <span class="navbar-text mx-2" id="showCountDownDate"></span>
             <form class="d-flex my-2 my-lg-0" role="search" action="<?=FEATURES . 'signout.php'?>" method="get">
                 <button class="btn btn-sm btn-outline-secondary" type="submit">Sign Out</button>
             </form>
         </div>
     </div>
 </nav>
+<input id="sessionExpire" type="hidden" value=<?=$_SESSION["expire"] *1000; ?>>
+<script>
+  var countDownDate = document.getElementById("sessionExpire").value;;
+  var x = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById("showCountDownDate").innerHTML = minutes + ":" + seconds;
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("showCountDownDate").innerHTML = "EXPIRED";
+        }
+    }, 1000);
+</script>

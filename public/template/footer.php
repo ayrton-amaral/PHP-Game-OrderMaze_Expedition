944cbc79eaa23153e51b5f1c2c38cc3633af1ff4
gabriellamiwa <gabriellakaku@gmail.com>
2023-12-02 18:25:58 -0500

    Update footer.php

---
+++ public/template/footer.php
@@ -0,0 +1,10 @@
+
+<footer class="py-3 my-4 " >
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Ayrton Senna Seraphim Do Amaral</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Bruno Lemos Landeiro</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Gabriella Miwa Mori Kaku</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Karina Piekarski Siviero Contini</a></li>
+    </ul>
+    <p class="text-center text-muted">© 2023 OrderMaze Expedition</p>
+  </footer>
